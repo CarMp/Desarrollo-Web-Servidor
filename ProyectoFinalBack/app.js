de8d8c8db903046dcf040363c4321ced97699392
Juan <jc.mendivil23@info.uas.edu.mx>
2025-05-30 23:21:31 -0700

    se agregan rutas crud a la api

--- ProyectoFinalBack/app.js
+++ ProyectoFinalBack/app.js
@@ -14,7 +14,7 @@ app.listen(puerto, () => {
     console.log('servicio iniciado')
 })
 
-app.post('/convertir/', async (req, res) => {
+app.post('/convertir', async (req, res) => {
     const { origen, destino, cantidad } = req.body;
     let resultado = 0;
     
@@ -41,10 +41,36 @@ app.post('/convertir/', async (req, res) => {
 
 })
 
-app.get('/monedas/', async (req, res) => {
-
+// Obtener todas las monedas
+app.get('/monedas', async (req, res) => {
     const data = await monedas.findAll();
+    if (!data || data.length === 0) return res.sendStatus(404);
+    res.send(data);
+});
 
-    res.send(data)
+// Crear una nueva moneda
+app.post('/crear', async (req, res) => {
+    const { origen, destino, valor } = req.body;
+    const nuevaMoneda = await monedas.create({ origen, destino, valor });
+    res.send(nuevaMoneda);
+});
 
-})
+// Actualizar una moneda por ID
+app.put('/actualizar/:id', async (req, res) => {
+    const { id } = req.params;
+    const { origen, destino, valor } = req.body;
+    const [actualizados] = await monedas.update(
+        { origen, destino, valor },
+        { where: { id } }
+    );
+    if (!actualizados) return res.sendStatus(404);
+    res.send({ id, origen, destino, valor });
+});
+
+// Eliminar una moneda por ID
+app.delete('/eliminar/:id', async (req, res) => {
+    const { id } = req.params;
+    const eliminados = await monedas.destroy({ where: { id } });
+    if (!eliminados) return res.sendStatus(404);
+    res.send({ mensaje: `Moneda con ID ${id} eliminada` });
+});
